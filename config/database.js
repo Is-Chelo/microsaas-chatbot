664593693cd8ed9a82d14c603235a0404cac7cff
Chelo <carlosmarcelotorresvargas@gmail.com>
2026-02-05 10:00:18 -0400

    fix

--- config/database.js
+++ config/database.js
@@ -1,16 +1,8 @@
 require('dotenv').config();
-// module.exports = {
-// 	username: process.env.MYSQL_USER || 'root',
-// 	password: process.env.MYSQL_PASSWORD || null,
-// 	database: process.env.MYSQL_DB_NAME || 'lab',
-// 	host: process.env.MYSQL_HOST || '127.0.0.1',
-// 	dialect: process.env.DIALECT || 'mysql',
-// };
 module.exports = {
-	username: process.env.MYSQLUSER || 'root',
-	password: process.env.MYSQLPASSWORD || 'fMFCKdrpQHsSVHcUfQEjyBFztHfoSlZN',
-	database: process.env.MYSQLDATABASE || 'railway',
-	host: process.env.MYSQLHOST || 'mysql.railway.internal',
-	port: process.env.MYSQLPORT || '3306',
+	username: process.env.MYSQL_USER || 'root',
+	password: process.env.MYSQL_PASSWORD || null,
+	database: process.env.MYSQL_DB_NAME || 'lab',
+	host: process.env.MYSQL_HOST || '127.0.0.1',
 	dialect: process.env.DIALECT || 'mysql',
 };
